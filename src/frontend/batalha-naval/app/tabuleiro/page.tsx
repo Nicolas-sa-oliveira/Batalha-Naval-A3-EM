e13624aaf75caebb998f109bd062af44324becd1
Nicolas Sá Oliveira <146349110+Nicolas-sa-oliveira@users.noreply.github.com>
2024-12-09 23:44:41 -0300

    adeus token

--- src/frontend/batalha-naval/app/tabuleiro/page.tsx
+++ src/frontend/batalha-naval/app/tabuleiro/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
 
     const fetchTabuleiros = async () => {
       try {
-        const url = `https://web-production-cc859.up.railway.app/partida/tabuleiro/${id}/${nomeJogador}/?token=${token}`;
+        const url = `https://web-production-cc859.up.railway.app/partida/tabuleiro/${id}/${nomeJogador}`;
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error('Erro ao buscar os tabuleiros');
@@ -104,7 +104,7 @@ export default function Home() {
     }
 
     try {
-      const url = `https://web-production-cc859.up.railway.app/partida/tabuleiro/peças/${id}/${nomeJogador}/${embarcacao}/${coordX}/${coordY}/${orientacao}/?token=${token}`;
+      const url = `https://web-production-cc859.up.railway.app/partida/tabuleiro/peças/${id}/${nomeJogador}/${embarcacao}/${coordX}/${coordY}/${orientacao}`;
       const response = await fetch(url, {
         method: 'PATCH',
         headers: {
@@ -119,7 +119,7 @@ export default function Home() {
         throw new Error('Erro ao enviar os dados para a API');
       }
       const updatedResponse = await fetch(
-        `https://web-production-cc859.up.railway.app/partida/tabuleiro/${id}/${nomeJogador}/?token=${token}`
+        `https://web-production-cc859.up.railway.app/partida/tabuleiro/${id}/${nomeJogador}`
       );
       const updatedData: string[][] = await updatedResponse.json();
       const tabuleirosProcessados = updatedData.map(tabuleiro =>
